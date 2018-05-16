67f95d52fd8d20fc910efcee99149a03e4e08118
ethan510010 <ethan510010@hotmail.com>
2018-05-16 09:45:51 +0800

    Ethan_Soj

---
+++ Ethan_Soj/MultiGame/MultiGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiGame
+//
+//  Created by EthanLin on 2018/5/16.
+//  Copyright © 2018 EthanLin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
