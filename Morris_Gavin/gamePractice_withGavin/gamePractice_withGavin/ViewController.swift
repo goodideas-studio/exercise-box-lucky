aca1a0626b4d41db15c2276e4500e3ef544575b4
Morris <morris.lwt@gmail.com>
2018-05-16 09:48:06 +0800

    First Commit

---
+++ Morris_Gavin/gamePractice_withGavin/gamePractice_withGavin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gamePractice_withGavin
+//
+//  Created by Morris on 2018/5/16.
+//  Copyright © 2018年 Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
